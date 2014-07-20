e6bed30fc3f3ed7c2898130ee5d47cd924b11b54
RamiTrabelsi <trabelsi.med.rami@gmail.com>
2014-07-20 18:48:55 +0100

    conflict finally resolved

--- Sa7i7AlBoukhari/src/com/sa7i7alboukhari/MainActivity.java
+++ Sa7i7AlBoukhari/src/com/sa7i7alboukhari/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sa7i7alboukhari;
 
 import android.annotation.SuppressLint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,13 +12,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.sa7i7alboukhari.AhadithSearchDialog.EditNameDialogListener;
 import com.sa7i7alboukhari.adapters.IMenuListener;
 import com.sa7i7alboukhari.adapters.MenuCustomAdapter;
 import com.sa7i7alboukhari.externals.SABDataBase;
@@ -26,20 +30,22 @@ import com.sa7i7alboukhari.utils.MySuperScaler;
 
 
 @SuppressLint({ "Recycle", "HandlerLeak" })
-public class MainActivity extends MySuperScaler implements IMenuListener{
+public class MainActivity extends MySuperScaler implements IMenuListener, OnTouchListener, 
+
+EditNameDialogListener{
+
 
 
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
+	private Button btn_menu, btn_search;
 
 	public SABDataBase sabDB;
 
 	private ActionBarDrawerToggle mDrawerToggle;
 	RelativeLayout mainView ;
-	
+
 	public static final int MESSAGE_START = 1;
-	
-	boolean test ;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +59,9 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 
 		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 
-		MenuCustomAdapter adapter = new MenuCustomAdapter(this, getResources().obtainTypedArray(R.array.menu_drawables));
+		MenuCustomAdapter adapter = new MenuCustomAdapter(this, getResources
+
+				().obtainTypedArray(R.array.menu_drawables));
 
 
 		mDrawerList.setAdapter(adapter);
@@ -63,7 +71,9 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 
 		mainView = (RelativeLayout) findViewById(R.id.content_frame);
 
-		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.app_name, R.string.app_name) {
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 
+
+				R.drawable.ic_drawer, R.string.app_name, R.string.app_name) {
 			public void onDrawerClosed(View view) {
 				supportInvalidateOptionsMenu();
 			}
@@ -82,22 +92,25 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 		};
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-//		if (savedInstanceState == null) {
-//			selectItem(1);
-//		}
+		//		if (savedInstanceState == null) {
+		//			selectItem(1);
+		//		}
 
-		Button btn = (Button) findViewById(R.id.menu);
+		btn_menu = (Button) findViewById(R.id.menu);
+		btn_menu.setOnTouchListener(this);
+		//		btn_menu.setOnClickListener(new OnClickListener() {
+		//
+		//			@Override
+		//			public void onClick(View arg0) {
+		//				if(!mDrawerLayout.isDrawerOpen(Gravity.RIGHT))
+		//					mDrawerLayout.openDrawer(Gravity.RIGHT);
+		//				else
+		//					mDrawerLayout.closeDrawer(Gravity.RIGHT);		
+		//			}
+		//		});
 
-		btn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				if(!mDrawerLayout.isDrawerOpen(Gravity.RIGHT))
-					mDrawerLayout.openDrawer(Gravity.RIGHT);
-				else
-					mDrawerLayout.closeDrawer(Gravity.RIGHT);		
-			}
-		});
+		btn_search = (Button) findViewById(R.id.search);
+		btn_search.setOnTouchListener(this);
 
 	}
 
@@ -108,20 +121,20 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 		if(sabDB == null){
 			sabDB = new SABDataBase(this);
 		}
-		
+
 	}
-	
-	
+
+
 	@Override
 	protected void onStart() {
 		super.onStart();
 
 		Message msg = Message.obtain();
 		msg.what = MESSAGE_START;
-	    mHandler.sendMessageDelayed(msg, 10);
+		mHandler.sendMessageDelayed(msg, 10);
 	}
-	
-	
+
+
 	@Override
 	protected void onStop() {
 		super.onStop();
@@ -134,7 +147,9 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 
 	private class DrawerItemClickListener implements ListView.OnItemClickListener {
 		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+
+		{
 
 			selectItem(position);
 
@@ -178,7 +193,8 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 		//        sabDB.getAllHadithsWithPage(0);
 		//        sabDB.getAllHadithsWithBabId(6);
 		//        sabDB.getFavoriteHadiths();
-		//        sabDB.searchHadithWithText("Ø¹ÙŽÙ†Ù’ Ø¹Ù�ÙƒÙ’Ø±Ù�Ù…ÙŽØ©ÙŽ Ø¨Ù’Ù†Ù� Ø®ÙŽØ§Ù„Ù�Ø¯Ù�");
+		//        sabDB.searchHadithWithText("Ø¹ÙŽÙ†Ù’ Ø¹Ù�ÙƒÙ’Ø±Ù�Ù…ÙŽØ©ÙŽ Ø¨Ù’Ù†Ù� 
+
 		//        boolean isFav = sabDB.isHadithFavorite(4);
 		//        Log.i("", "isFav " + isFav);
 	}
@@ -204,7 +220,8 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 	public void onMenuItemClicked(int position) {
 		selectItem(position);
 	}
-	
+
+
 	private Handler mHandler = new Handler() {
 		public void handleMessage(Message msg) {
 
@@ -212,9 +229,80 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 			case MESSAGE_START :
 				selectItem(1);
 				break;
-			
+
 			}
 			super.handleMessage(msg);
-		
-	}};
+
+		}};
+
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN: {
+				Button view = (Button) v;
+				view.getBackground().setColorFilter(0x77000000, 
+
+						PorterDuff.Mode.SRC_ATOP);
+				v.invalidate();
+				break;
+			}
+			case MotionEvent.ACTION_UP: {
+
+				switch (v.getId()) {
+				case R.id.menu:
+					if(!mDrawerLayout.isDrawerOpen(Gravity.RIGHT))
+						mDrawerLayout.openDrawer(Gravity.RIGHT);
+					else
+						mDrawerLayout.closeDrawer(Gravity.RIGHT);		
+					break;
+				case R.id.search:
+					//show search dialog;
+					showSearchDialog();
+					break;
+				default:
+					break;
+				}
+
+			}
+			case MotionEvent.ACTION_CANCEL: {
+				Button view = (Button) v;
+				view.getBackground().clearColorFilter();
+				view.invalidate();
+				break;
+			}
+			}
+			return true;
+		}
+
+		private void showSearchDialog() {
+			FragmentManager fm = getSupportFragmentManager();
+			AhadithSearchDialog searchDialog = new AhadithSearchDialog();
+			searchDialog.show(fm, "fragment_search_keyword");
+		}
+
+		@Override
+		public void onFinishEditDialog(String inputText) {
+
+			Toast.makeText(this, getString(R.string.to_search) + " " + inputText, 
+
+					Toast.LENGTH_SHORT).show();
+
+			// update the main content by replacing fragments
+			Fragment fragment = new AhadithFragment();
+			Bundle args = new Bundle();
+			args.putInt(AhadithFragment.ARG_AHADITH, AhadithFragment.ARG_AHADITH_KEYWORD_ID);
+			args.putString(AhadithFragment.ARG_AHADITH_KEYWORD_TEXT, inputText);
+			fragment.setArguments(args);
+
+			FragmentManager fragmentManager = getSupportFragmentManager();
+			FragmentTransaction ft = fragmentManager.beginTransaction();
+
+			ft.replace(R.id.content_frame, fragment);
+			scaled = false ;
+			ft.commit();
+
+		}
+
+
 }
