336efb4f00cf4f140b8bcd5fed81cbba3cbe92e8
Hichem LAROUSSI <hichemlaroussi@gmail.com>
2014-07-20 16:29:42 +0100

    Search Ahadith done but ...
    To check how to enter arabic text with ponctuation

--- Sa7i7AlBoukhari/src/com/sa7i7alboukhari/MainActivity.java
+++ Sa7i7AlBoukhari/src/com/sa7i7alboukhari/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sa7i7alboukhari;
 
 import android.annotation.SuppressLint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
@@ -9,13 +10,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.sa7i7alboukhari.AhadithSearchDialog.EditNameDialogListener;
 import com.sa7i7alboukhari.adapters.IMenuListener;
 import com.sa7i7alboukhari.adapters.MenuCustomAdapter;
 import com.sa7i7alboukhari.externals.SABDataBase;
@@ -23,11 +27,12 @@ import com.sa7i7alboukhari.utils.MySuperScaler;
 
 
 @SuppressLint("Recycle")
-public class MainActivity extends MySuperScaler implements IMenuListener{
+public class MainActivity extends MySuperScaler implements IMenuListener, OnTouchListener, EditNameDialogListener{
 
 
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
+	private Button btn_menu, btn_search;
 
 	public SABDataBase sabDB;
 	
@@ -80,18 +85,21 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 			selectItem(1);
 		}
 
-		Button btn = (Button) findViewById(R.id.menu);
-
-		btn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				if(!mDrawerLayout.isDrawerOpen(Gravity.RIGHT))
-					mDrawerLayout.openDrawer(Gravity.RIGHT);
-				else
-					mDrawerLayout.closeDrawer(Gravity.RIGHT);		
-			}
-		});
+		btn_menu = (Button) findViewById(R.id.menu);
+		btn_menu.setOnTouchListener(this);
+//		btn_menu.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				if(!mDrawerLayout.isDrawerOpen(Gravity.RIGHT))
+//					mDrawerLayout.openDrawer(Gravity.RIGHT);
+//				else
+//					mDrawerLayout.closeDrawer(Gravity.RIGHT);		
+//			}
+//		});
+		
+		btn_search = (Button) findViewById(R.id.search);
+		btn_search.setOnTouchListener(this);
 
 	}
 	
@@ -118,9 +126,7 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
 			selectItem(position);
-
 
 		}
 	}
@@ -159,6 +165,71 @@ public class MainActivity extends MySuperScaler implements IMenuListener{
 	@Override
 	public void onMenuItemClicked(int position) {
 		selectItem(position);
+	}
+	
+	
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN: {
+			Button view = (Button) v;
+			view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+			v.invalidate();
+			break;
+		}
+		case MotionEvent.ACTION_UP: {
+			
+			switch (v.getId()) {
+			case R.id.menu:
+				if(!mDrawerLayout.isDrawerOpen(Gravity.RIGHT))
+					mDrawerLayout.openDrawer(Gravity.RIGHT);
+				else
+					mDrawerLayout.closeDrawer(Gravity.RIGHT);		
+				break;
+			case R.id.search:
+				//show search dialog;
+				showSearchDialog();
+				break;
+			default:
+				break;
+			}
+			
+		}
+		case MotionEvent.ACTION_CANCEL: {
+			Button view = (Button) v;
+			view.getBackground().clearColorFilter();
+			view.invalidate();
+			break;
+		}
+		}
+		return true;
+	}
+
+	 private void showSearchDialog() {
+	        FragmentManager fm = getSupportFragmentManager();
+	        AhadithSearchDialog searchDialog = new AhadithSearchDialog();
+	        searchDialog.show(fm, "fragment_search_keyword");
+	    }
+	
+	@Override
+	public void onFinishEditDialog(String inputText) {
+		
+		 Toast.makeText(this, getString(R.string.to_search) + " " + inputText, Toast.LENGTH_SHORT).show();
+
+		// update the main content by replacing fragments
+		Fragment fragment = new AhadithFragment();
+		Bundle args = new Bundle();
+		args.putInt(AhadithFragment.ARG_AHADITH, AhadithFragment.ARG_AHADITH_KEYWORD_ID);
+		args.putString(AhadithFragment.ARG_AHADITH_KEYWORD_TEXT, inputText);
+		fragment.setArguments(args);
+
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction ft = fragmentManager.beginTransaction();
+
+		ft.replace(R.id.content_frame, fragment);
+		scaled = false ;
+		ft.commit();
+
 	}
 
 }
